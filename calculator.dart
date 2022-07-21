1fa0f2beeff27ed542b7eafdcb4c8a0dfc13ea3a
marawan emad <88719313+Marawanemad@users.noreply.github.com>
2022-07-21 14:38:26 +0200

    Update calculator.dart

--- calculator.dart
+++ calculator.dart
@@ -1,31 +1,45 @@
 import 'dart:io';
-import 'dart:math';
 
 void main(List<String> arguments) {
-  print("Enter the first number: ");
-  double n1 = double.parse(stdin.readLineSync()!);
-  print("Enter the operation sign: ");
-  String n = stdin.readLineSync()!;
-  print("Enter the second number: ");
-  double n2 = double.parse(stdin.readLineSync()!);
-  switch (n) {
-    case "+":
-      print("$n1 + $n2 = ${n1 + n2}");
-      break;
-    case "*":
-      print("$n1 * $n2 = ${n1 * n2}");
-      break;
-    case "-":
-      print("$n1 - $n2 = ${n1 - n2}");
-      break;
-    case "/":
-      print("$n1 / $n2 = ${n1 / n2}");
-      break;
-    case "^":
-      print("$n1 ^ $n2 = ${pow(n1, n2)}");
-      break;
-    default:
-      print("the operation sign is uncorrect try again");
-      break;
+  int i = 0;
+  while (i != 5) {
+    print("Enter the number of the operation you want \n 1.add \n 2.sub \n 3.multiply \n 4.divid \n 5.exit ");
+    int n = int.parse(stdin.readLineSync()!);
+    switch (n) {
+      case 1:
+        print("Enter the first number: ");
+        double n1 = double.parse(stdin.readLineSync()!);
+        print("Enter the second number: ");
+        double n2 = double.parse(stdin.readLineSync()!);
+        print("$n1 + $n2 = ${n1 + n2}");
+        break;
+      case 3:
+        print("Enter the first number: ");
+        double n1 = double.parse(stdin.readLineSync()!);
+        print("Enter the second number: ");
+        double n2 = double.parse(stdin.readLineSync()!);
+        print("$n1 * $n2 = ${n1 * n2}");
+        break;
+      case 2:
+        print("Enter the first number: ");
+        double n1 = double.parse(stdin.readLineSync()!);
+        print("Enter the second number: ");
+        double n2 = double.parse(stdin.readLineSync()!);
+        print("$n1 - $n2 = ${n1 - n2}");
+        break;
+      case 4:
+        print("Enter the first number: ");
+        double n1 = double.parse(stdin.readLineSync()!);
+        print("Enter the second number: ");
+        double n2 = double.parse(stdin.readLineSync()!);
+        print("$n1 / $n2 = ${n1 / n2}");
+        break;
+      case 5:
+        i = n;
+        break;
+      default:
+        print("the operation sign is uncorrect try again");
+        break;
+    }
   }
 }
